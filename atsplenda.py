223388f7f2d4fa3a4b7bcba96340ddb737cbecaf
BuildTools <unconfigured@null.spigotmc.org>
2019-03-27 13:07:02 -0500

    @spnexa? whos spnexa?

--- atsplenda.py
+++ atsplenda.py
@@ -13,28 +13,30 @@ async def on_message(message):
 
     if "@splenda" in message.content.lower():
         msg = '<@385933420389335061>'.format(message)
-        #await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
+
+    if "@spnexa" in message.content.lower():
+        msg = 'you wish'.format(message)
         await message.channel.send(msg)
 
     if message.content.startswith('bitch'):
         msg = 'lasagna'.format(message)
-        #await client.send_message(message.channel, msg)
         await message.channel.send(msg)
 
     if message.content.startswith(':))))'):
         msg = 'Woah, calm yo tits there. no one has that many chins'.format(message)
-        #await client.send_message(message.channel, msg)
         await message.channel.send(msg)
 
     if message.content.startswith('XD'):
         msg = 'lulz'.format(message)
-        #await client.send_message(message.channel, msg)
         await message.channel.send(msg)
 
     if message.content.startswith('D:'):
         msg = 'shocking'.format(message)
         #await client.send_message(message.channel, msg)
         await message.channel.send(msg)
+
+
 
 @client.event
 async def on_ready():
